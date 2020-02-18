ab8f52420208132db309d6420e6d04217c7978c3
cc <chengchen.chn@outlook.com>
2020-02-18 19:42:46 +0800

    WIP: update killss

--- ss-config/killss.sh
+++ ss-config/killss.sh
@@ -1,3 +1,12 @@
 #!/bin/zsh
 
-ps -ef | grep ss-local | awk 'NR==1 {print $2}' | xargs kill
+ss=$(ps -ef | grep ss-local | awk 'NR==1 {print $0}')
+if [[ $ss =~ "grep" ]]
+then
+	echo "no ss-local process"
+else
+	ss_port=$(echo $ss | awk 'NR==1 {print $2}')
+	echo "kill ss-local process"
+	echo "port $ss_port"
+	kill -9 $ss_port
+fi
